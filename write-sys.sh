30203ae4f948b019e87ea058f2ca12af0dc0f274
Michael Weiss <github@mishafarms.us>
2020-10-06 17:14:26 -0700

    Changed system flash to use a script, that reads the partition table and writes to the vfs partition, mp.bin is now the latest micropython GENERIC build with a partition table that has an eeprom partition so we don't lose the network settings write-sys.sh is the new script that will read the partition and write the sys.img to it. sys.img is slightly larger and fits the vfs partition exactly for our micropython binary partition table

---
+++ write-sys.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while getopts p:b: flag
+	do
+	    case "${flag}" in
+	        p) PORT=${OPTARG};;
+	        b) BAUD=${OPTARG};;
+	    esac
+	done
+if [[ -z "$PORT" ]]; then
+	if [[ -z "${ESPPORT}" ]]; then
+		PORT="/dev/ttyUSB0"
+	else
+		PORT="${ESPPORT}"
+	fi
+fi
+
+if [[ -z "$BAUD" ]]; then
+	if [[ -z "${ESPBAUD}" ]]; then
+		BAUD="115200"
+	else
+		BAUD="${ESPBAUD}"
+	fi
+fi
+ 
+esptool.py --chip esp32 -b 115200 -p $PORT read_flash 0x8000 0xc00 ptable.img
+PART_ADDR=`gen_esp32part.py ./ptable.img | grep vfs | awk -F',' '{print $4}'`
+rm ptable.img
+echo "writing to port " ${PORT} " baud = " ${BAUD} " address = " ${PART_ADDR}
+esptool.py --chip esp32 -b ${BAUD} -p ${PORT} write_flash -z ${PART_ADDR} images/sys.img

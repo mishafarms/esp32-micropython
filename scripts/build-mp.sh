dcb9db5ddf44dc586c92ace1847b80e3adfab675
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-03-18 11:52:07 -0700

    Update build-mp.sh

--- scripts/build-mp.sh
+++ scripts/build-mp.sh
@@ -39,4 +39,7 @@ WD=$(pwd)
  cp build-WROVER_16M-OTA_USER/micropython.bin "${WD}/images" &&
  cp build-WROVER_16M-OTA_USER/partition_table/partition-table.bin "${WD}/images" &&
  cp build-WROVER_16M-OTA_USER/bootloader/bootloader.bin "${WD}/images" &&
- cp build-WROVER_16M-OTA_USER/ota_data_initial.bin "${WD}/images")
+ cp build-WROVER_16M-OTA_USER/ota_data_initial.bin "${WD}/images" && cd "${WD}" &&
+ PART_ADDR=$(./gen_esp32part.py images/partition-table.bin | grep vfs | awk -F',' '{print $4}') &&
+ esptool.py --chip esp32 merge_bin -o images/merged-firmware.bin --flash_mode dio --flash_size 16MB --flash_freq 40m 0x1000 images/bootloader.bin 0x8000 images/partition-table.bin 0x10000 images/micropython.bin 0x510000 images/ota_data_initial.bin "${PART_ADDR}" images/sys.img
+)
